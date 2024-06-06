91b64e68299972f33fcf0121290df5a11abbffc4
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-06-06 12:40:04 +0530

    adv

--- src/components/App.js
+++ src/components/App.js
@@ -31,50 +31,49 @@ import Step from "./Step";
 const App = () => {
   const [step, setStep] = useState(1);
 
-  const handleButtonClick = (e, action) => {
+  function handleButtonClick(e, action) {
     e.preventDefault();
-    if (action === "next") {
+    if (action === "next" && step < 3) {
       setStep(step + 1);
-    } else if (action === "previous") {
+    } else if (action === "previous" && step > 1) {
       setStep(step - 1);
     }
-  };
-
+  }
   return (
     <div>
       {step === 1 && (
-        <Step 
-          title="Customer Details"
-          id1="first_name"
-          id2="last_name"
-          placeholder1="First Name"
-          placeholder2="Last Name"
-          handleButtonClick={handleButtonClick}
-          step="step1"
-        />
-      )}
-      {step === 2 && (
-        <Step 
-          title="Car Details"
-          id1="model"
-          id2="car_price"
-          placeholder1="Car Model"
-          placeholder2="Car Price"
-          handleButtonClick={handleButtonClick}
-          step="step2"
-        />
-      )}
-      {step === 3 && (
-        <Step 
-          title="Payment Details"
-          id1="card_info"
-          id2="expiry_date"
-          placeholder1="Card Info"
-          placeholder2="Expiry Date"
-          handleButtonClick={handleButtonClick}
-          step="step3"
-        />
-      )}
+          <Step 
+            title="Customer Details"
+            id1="first_name"
+            id2="last_name"
+            placeholder1="First Name"
+            placeholder2="Last Name"
+            handleButtonClick={handleButtonClick}
+            step="step1"
+          />
+        )}
+        {step === 2 && (
+          <Step 
+            title="Car Details"
+            id1="model"
+            id2="car_price"
+            placeholder1="Car Model"
+            placeholder2="Car Price"
+            handleButtonClick={handleButtonClick}
+            step="step2"
+          />
+        )}
+        {step === 3 && (
+          <Step 
+            title="Payment Details"
+            id1="card_info"
+            id2="expiry_date"
+            placeholder1="Card Info"
+            placeholder2="Expiry Date"
+            handleButtonClick={handleButtonClick}
+            step="step3"
+          />
+        )}
     </div>
   );
 };

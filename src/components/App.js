2205a0088df204574a6b2b7684b9f8d7607205ac
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-06-06 12:33:16 +0530

    new commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,82 @@
 
+// import React, { useState } from "react";
+// import './../styles/App.css';
+// import Step from "./Step";
+
+// const App = () => {
+//   const [currStep,setCurrStep]=useState({previous:false,next:true,submit:false});
+//   function handleCurrentState(e,curr){
+//     e.preventDefault();
+//     setCurrStep(curr);
+
+//   }
+//   return (
+//     <div>
+//        {( currStep.previous==false && currStep.submit==false)?
+//         <Step title="Customer Details"  id1="first-name" id2="last_name" placeholder1="first-name" placeholder2="last-nmae"  handleCurrentState={handleCurrentState} step="step1"/>:
+//        ( currStep.previous==true && currStep.next==true)? <Step title="Car Details" id1="model" id2="car_price" placeholder1="Brand" placeholder2="Model"  handleCurrentState={handleCurrentState} step="step2"/>:
+//         <Step title="Payment Details" id1="card_info" id2="expiry_date" placeholder1="Card Info" placeholder2="Expiry Date"  handleCurrentState={handleCurrentState} step="step3"/>
+
+//        }
+//     </div>
+//   )
+// }
+
+// export default App
+
+
 import React, { useState } from "react";
-import './../styles/App.css';
 import Step from "./Step";
 
 const App = () => {
-  const [currStep,setCurrStep]=useState({previous:false,next:true,submit:false});
-  function handleCurrentState(e,curr){
-    e.preventDefault();
-    setCurrStep(curr);
+  const [step, setStep] = useState(1);
 
-  }
+  const handleButtonClick = (e, action) => {
+    e.preventDefault();
+    if (action === "next") {
+      setStep(step + 1);
+    } else if (action === "previous") {
+      setStep(step - 1);
+    }
+  };
+
   return (
     <div>
-       {( currStep.previous==false && currStep.submit==false)?
-        <Step title="Customer Details"  id1="first-name" id2="last_name" placeholder1="first-name" placeholder2="last-nmae"  handleCurrentState={handleCurrentState} step="step1"/>:
-       ( currStep.previous==true && currStep.next==true)? <Step title="Car Details" id1="model" id2="car_price" placeholder1="Brand" placeholder2="Model"  handleCurrentState={handleCurrentState} step="step2"/>:
-        <Step title="Payment Details" id1="card_info" id2="expiry_date" placeholder1="Card Info" placeholder2="Expiry Date"  handleCurrentState={handleCurrentState} step="step3"/>
-
-       }
+      {step === 1 && (
+        <Step 
+          title="Customer Details"
+          id1="first_name"
+          id2="last_name"
+          placeholder1="First Name"
+          placeholder2="Last Name"
+          handleButtonClick={handleButtonClick}
+          step="step1"
+        />
+      )}
+      {step === 2 && (
+        <Step 
+          title="Car Details"
+          id1="model"
+          id2="car_price"
+          placeholder1="Car Model"
+          placeholder2="Car Price"
+          handleButtonClick={handleButtonClick}
+          step="step2"
+        />
+      )}
+      {step === 3 && (
+        <Step 
+          title="Payment Details"
+          id1="card_info"
+          id2="expiry_date"
+          placeholder1="Card Info"
+          placeholder2="Expiry Date"
+          handleButtonClick={handleButtonClick}
+          step="step3"
+        />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Step.js
+++ src/components/Step.js
@@ -1,3 +1,58 @@
+// import React from "react";
+
+// const Step = (props) => {
+//   return (
+//     <form id={props.step}>
+//       <h1>{props.title}</h1>
+//       <input id={props.id1} placeholder={props.placeholder1}></input>
+//       <br />
+//       <input id={props.id2} placeholder={props.placeholder2}></input>
+//       <br></br>
+//       {props.step == "step1" ? (
+//         <button
+//           onClick={(e) =>
+//             props.handleCurrentState(e,{
+//               previous: true,
+//               next: true,
+//               submit: false,
+//             })
+//           }
+//         >
+//           Next
+//         </button>
+//       ) : props.step == "step2" ? (
+//         <>
+//           <button
+//             onClick={(e) =>
+//               props.handleCurrentState(e,{
+//                 previous: false,
+//                 next: true,
+//                 submit: false,
+//               })
+//             }
+//           >
+//             previous
+//           </button>
+//           <button
+//             onClick={(e) =>
+//               props.handleCurrentState(e,{ previous: true, next: false,submit:true })
+//             }
+//           >
+//             next
+//           </button>
+//         </>
+//       ) : (
+//         <>
+//           <button onClick={(e)=>props.handleCurrentState(e,{previous:true,next:true,submit:false})}>previous</button>
+//           <button onClick={(e)=>e.preventDefault()}>submit </button>
+//         </>
+//       )}
+//     </form>
+//   );
+// };
+
+// export default Step;
+
 import React from "react";
 
 const Step = (props) => {
@@ -8,44 +63,14 @@ const Step = (props) => {
       <br />
       <input id={props.id2} placeholder={props.placeholder2}></input>
       <br></br>
-      {props.step == "step1" ? (
-        <button
-          onClick={(e) =>
-            props.handleCurrentState(e,{
-              previous: true,
-              next: true,
-              submit: false,
-            })
-          }
-        >
-          Next
-        </button>
-      ) : props.step == "step2" ? (
-        <>
-          <button
-            onClick={(e) =>
-              props.handleCurrentState(e,{
-                previous: false,
-                next: true,
-                submit: false,
-              })
-            }
-          >
-            previous
-          </button>
-          <button
-            onClick={(e) =>
-              props.handleCurrentState(e,{ previous: true, next: false,submit:true })
-            }
-          >
-            next
-          </button>
-        </>
-      ) : (
-        <>
-          <button onClick={(e)=>props.handleCurrentState(e,{previous:true,next:true,submit:false})}>previous</button>
-          <button onClick={(e)=>e.preventDefault()}>submit </button>
-        </>
+      {props.step !== "step3" && (
+        <button onClick={(e) => props.handleButtonClick(e, "next")}>Next</button>
+      )}
+      {props.step !== "step1" && (
+        <button onClick={(e) => props.handleButtonClick(e, "previous")}>Previous</button>
+      )}
+      {props.step === "step3" && (
+        <button onClick={(e) => e.preventDefault()}>Submit</button>
       )}
     </form>
   );
